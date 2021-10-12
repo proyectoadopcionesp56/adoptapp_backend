a0c7aa01272d99d73385d5362fef9e792a434be7
ccgarcia <ccgarcia@itc.com.co>
2021-10-11 22:36:42 -0500

    Registro de proyecto adoptapp hasta lo requerido para el Sprint 3

--- authApp/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pet import Pet
-from .user import User
-from .requestPet import RequestPet
--- authApp/serializers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .petSerializer import PetSerializer
-from .userSerializer import UserSerializer
-from .requestPetSerializer import RequestPetSerializer
