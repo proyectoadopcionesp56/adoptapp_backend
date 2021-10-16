646ce04d9f39d1d80d57e466a953a963cc3de3a4
Jeisson Mendivelso <jeisson.santiesteban@uptc.edu.co>
2021-10-15 23:44:58 -0500

    feat(views): agregadas las vistas para el sprint 4

---
+++ authApp/views/__init__.py
@@ -0,0 +1,9 @@
+# create
+from .userCreateView import UserCreateView
+from .petCreateView import PetCreateView
+# details
+from .userDetailView import UserDetailView
+from .petDetailView import PetDetailView
+from .petDetailsView import PetDetailsView
+# put
+from .petUpdateView import PetUpdateView
