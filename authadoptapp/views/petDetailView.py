09891a1e776b34eed16f4184d16534894080136f
ccgarcia <ccgarcia@itc.com.co>
2021-10-17 09:57:23 -0500

    deployment changes

--- authadoptapp/views/petDetailView.py
+++ authadoptapp/views/petDetailView.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.backends import TokenBackend
-from rest_framework.permissions import IsAuthenticated
 from authadoptapp.models.pet import Pet
 from authadoptapp.serializers.petSerializer import PetSerializer
 
@@ -10,7 +9,6 @@ from authadoptapp.serializers.petSerializer import PetSerializer
 class PetDetailView(generics.RetrieveAPIView):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
-    permission_classes = (IsAuthenticated,)
-
+    
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
